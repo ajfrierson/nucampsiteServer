f9b8efa354ffc81631e8285f3131fd348908e353
Alvin <huerox@msn.com>
2020-09-01 19:02:18 -0700

    passport facebook.

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '3546714918696424',
+        clientSecret: '703b40fda63488fe7914f6806567dca6'
+    }
 }
